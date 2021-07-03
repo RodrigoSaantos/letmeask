f7b581ee1ad16070e022b68542798280768d2966
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-03 15:29:57 -0300

    Remove likes

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -12,7 +12,7 @@ type QuestionType = {
   isAnswered: boolean;
   isHighlighted: boolean;
   likeCount: number;
-  hasLiked: boolean;
+  likeId: string | undefined;
 }
 
 type FirebaseQuestions = Record<string, {
@@ -47,7 +47,7 @@ export function useRoom(roomId: string) {
           isHighlighted: value.isHighlighted,
           isAnswered: value.isAnswered,
           likeCount: Object.values(value.likes ?? {}).length,
-          hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
         }
       })
 
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -46,13 +46,13 @@ export function Room() {
     setNewQuestion('');
   }
 
-  async function handleLikeQuestion(questionId: string, hasLiked: boolean) {
-    if (hasLiked) {
-      // remover
+  async function handleLikeQuestion(questionId: string, likeId: string | undefined) {
+    if (likeId) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}/likes/${likeId}`).remove()
     } else {
       await database.ref(`rooms/${roomId}/questions/${questionId}/likes`).push({
         authorId: user?.id,
-      })
+      });
     }
   }
 
@@ -99,10 +99,10 @@ export function Room() {
                 key={question.id}
               >
                 <button 
-                  className={`like-button ${question.hasLiked ? 'liked' : ''}`} 
+                  className={`like-button ${question.likeId ? 'liked' : ''}`} 
                   type='button' 
                   aria-label="Marcar como gostei"
-                  onClick={() => handleLikeQuestion(question.id, question.hasLiked)}
+                  onClick={() => handleLikeQuestion(question.id, question.likeId)}
                 >
                   {question.likeCount > 0 && <span>{question.likeCount}</span>}
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
